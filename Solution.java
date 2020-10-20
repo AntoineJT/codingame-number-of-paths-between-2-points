050092dbf8cdc7f1cf7ba6ba7010ea1e6c37468c
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-10-20 17:15:25 +0200

    Optimize it even more

--- Solution.java
+++ Solution.java
@@ -41,13 +41,17 @@ class PathCalculator {
         if (walls.isWall(x, y) || walls.isWall(n - 1, m - 1))
             return 0;
 
+        // pre-compute some values and feed cache with it
         int[][] cache = new int[n][m];
-        // fill cache with "not cached" value
         for (int b = 0; b < m; ++b) {
             for (int a = 0; a < n; ++a) {
-                cache[a][b] = -1;
+                // if it is a wall, don't count this path as a valid one (0)
+                // else mark it as "uncached for now" (-1)
+                cache[a][b] = walls.isWall(a, b) ? 0 : -1;
             }
         }
+        // map fully explored
+        cache[n - 1][m - 1] = 1;
 
         return recNumberOfPaths(x, y, cache);
     }
@@ -62,6 +66,9 @@ class PathCalculator {
         if (cache[x][y] != -1)
             return cache[x][y];
 
+// those things are commented because they have been precalculated by numberOfPaths function
+// calling this function with empty cache will break it so
+/*
         // map fully explored
         if (x == n - 1 && y == m - 1)
             return 1;
@@ -69,6 +76,7 @@ class PathCalculator {
         // this is a wall, don't count this path as a valid one
         if (walls.isWall(x, y))
             return 0;
+*/
 
         // this is not cached, so let us cache it
         cache[x][y] = recNumberOfPaths(x + 1, y, cache) + recNumberOfPaths(x, y + 1, cache);
