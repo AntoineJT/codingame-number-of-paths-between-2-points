9ca1b0e32a86639eb0a477f18f22a0a5ab1ff73a
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-10-20 16:48:07 +0200

    Clean code

--- Solution.java
+++ Solution.java
@@ -10,22 +10,17 @@ class Solution {
             in.nextLine();
         }
         
-        // String dump = "";
         // populate walls map
         WallMap walls = new WallMap();
         for (int y = 0; y < M; ++y) {
             String ROW = in.nextLine();
-            int x = 0;
-            for (char c : ROW.toCharArray()) {
-                // dump += x + "," + y + " " + c + "\n";
-                walls.put(x, y, c == '1');
-                ++x;
+            for (int x = 0; x < N; ++x) {
+                walls.put(x, y, ROW.charAt(x) == '1');
             }
         }
         in.close();
-        // System.out.println(dump);
+        
         PathCalculator pc = new PathCalculator(walls, M, N);
-
         System.out.println(pc.numberOfPaths(0, 0));
     }
 }
@@ -43,11 +38,10 @@ class PathCalculator {
 
     public int numberOfPaths(int x, int y) {
         // if start point or end point is a wall, then no path exists
-        if (walls.isWall(x, y) || walls.isWall(n - 1, m - 1)) {
+        if (walls.isWall(x, y) || walls.isWall(n - 1, m - 1))
             return 0;
-        }
 
-       return recNumberOfPaths(x, y);
+        return recNumberOfPaths(x, y);
     }
 
     private int recNumberOfPaths(int x, int y) 
